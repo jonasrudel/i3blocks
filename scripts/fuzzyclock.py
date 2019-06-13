ab351a7ad3e6e56ba29ddc9a287835832bff779f
jonasx220 <jonru1@web.de>
2019-06-13 20:44:07 +0200

    moved fuzzyclock

---
+++ scripts/fuzzyclock.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import time
+
+hour=int(time.strftime("%I"))
+minute=int(time.strftime("%M"))
+
+texthours = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve"]
+
+
+textminutes = ["", "five past ", "ten past ", "fifteen past ", "twenty past ", "twentyfive past ", "half past ", "twentyfive to ", "twenty to ", "fifteen to ", "ten to ", "five to "]
+
+if minute > 30:
+    if hour < 12: 
+        hour+=1
+    else:
+        hour = 1
+if (minute > 57):
+    minute-=60
+
+minute+=2
+
+print(textminutes[int(minute/5)] + texthours[hour-1])
